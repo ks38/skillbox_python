efa61e77c7489e01d0cebed752a95d90021e3993
knyazevvb-PC-HOME <ks38@mail.ru>
2023-03-30 21:10:09 +0300

    6 tasks - module 4 done

--- module_4/main.py
+++ module_4/main.py
@@ -3,3 +3,9 @@
 
 # Что нужно сделать:
 # Зайдите в файлы с заданиями и исправте так чтобы вывод соотвествовал условиям.
+# import module_4.tasks.task_1
+# import module_4.tasks.task_2
+# import module_4.tasks.task_3
+# import module_4.tasks.task_4
+# import module_4.tasks.task_5
+import module_4.tasks.task_6
