fea2cea42f60f976cb5273b0e8181b5988e2c52d
knyazevvb-PC-HOME <ks38@mail.ru>
2023-03-30 10:09:14 +0300

    add module 3

---
+++ module_3/main.py
@@ -0,0 +1,14 @@
+# Цель задания
+# Научиться работать с базовым синтаксисом Python.
+
+# Что нужно сделать:
+# Зайдите в файлы с заданиями и выполните так чтобы вывод соотвествовал условиям.
+
+import tasks.task_1
+import tasks.task_2
+import tasks.task_3
+import tasks.task_4
+import tasks.task_5
+import tasks.task_6
+import tasks.task_7
+import tasks.task_8
---
+++ module_3/tasks/task_4.py
@@ -0,0 +1,11 @@
+print('Задача 4. Площадь треугольника')
+
+# Напишите программу,
+# которая запрашивает у пользователя длины двух катетов
+# в прямоугольном треугольнике и выводит его площадь.
+
+# Формула:
+# S = ab/2
+cathet_one = int(input("Enter first cathet: "))
+cathet_two = int(input("Enter second cathet: "))
+print("Square of triangle = ", (cathet_one * cathet_two) / 2)
