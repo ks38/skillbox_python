9fa0b21c7be9354dc4f24092c59c2dd06c2d636d
knyazevvb-PC-HOME <ks38@mail.ru>
2023-03-31 10:40:03 +0300

    module 4 ||  fixed main.py

--- module_4/main.py
+++ module_4/main.py
@@ -3,11 +3,11 @@
 
 # Что нужно сделать:
 # Зайдите в файлы с заданиями и исправте так чтобы вывод соотвествовал условиям.
-# import module_4.tasks.task_1
-# import module_4.tasks.task_2
-# import module_4.tasks.task_3
-# import module_4.tasks.task_4
-# import module_4.tasks.task_5
-# import module_4.tasks.task_6
+import module_4.tasks.task_1
+import module_4.tasks.task_2
+import module_4.tasks.task_3
+import module_4.tasks.task_4
+import module_4.tasks.task_5
+import module_4.tasks.task_6
 import module_4.tasks.task_7
-# import module_4.tasks.task_8
+import module_4.tasks.task_8
