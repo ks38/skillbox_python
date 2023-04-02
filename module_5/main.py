1e6730031e43401ad34a36e7a7165e46f1aaf943
Vyacheslav Knyazev <ks38@mail.ru>
2023-04-02 14:23:20 +0300

    imported tasks 1-7 in main.py | module 5

--- module_5/main.py
+++ module_5/main.py
@@ -3,3 +3,11 @@
 
 # Что нужно сделать:
 # Зайдите в файлы с заданиями и выполните так чтобы вывод соотвествовал условиям.
+
+import module_5.tasks.task_3
+import module_5.tasks.task_2
+import module_5.tasks.task_1
+import module_5.tasks.task_4
+import module_5.tasks.task_5
+import module_5.tasks.task_6
+import module_5.tasks.task_7
