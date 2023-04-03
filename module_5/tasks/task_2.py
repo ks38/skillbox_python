62b8d8b6081676b1f0650be53005a086a80682f3
jaggrenko <73942845+jaggrenko@users.noreply.github.com>
2023-04-03 22:19:01 +0300

    Update task_2.py
     print("y =", y) -- дублируешь; оставь один экземпляр

--- module_5/tasks/task_2.py
+++ module_5/tasks/task_2.py
@@ -24,10 +24,11 @@ x = int(input("Enter x: "))
 
 if x == 0:
     y = 5
-    print("y =", y)
+    # print("y =", y)
 elif x > 0:
     y = x - 12
     print("y =", y)
 else:
     y = x ** 2
-    print("y =", y)
+    
+print("y =", y)
